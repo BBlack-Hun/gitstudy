20422735a6d2d494fef4acdea92f3a8c0f38cea4
BBlack-Hun <55784870+BBlack-Hun@users.noreply.github.com>
2019-11-04 17:04:54 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,8 +1,13 @@
-public class Calc {
+public class Calc 
+{
+	public int r = 0;
 	public int add(int x, int y) { //add method
 		return x+y;
 	}
 	public int sub(int x, int y) {
 		return x -y;
+	}
+	public void dec(int d) {
+		r -=d;
 	}
 }
